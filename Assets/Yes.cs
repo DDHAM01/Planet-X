038bd2aabf78550ba5b03dabd7101b83ee1efff9
DDHAM01 <dhamidevin580@gmail.com>
2025-10-04 16:56:18 -0700

    Grid

---
+++ Assets/Yes.cs
@@ -0,0 +1,11 @@
+  using UnityEngine;
+
+    public class Yes : MonoBehaviour
+    {
+        void OnMouseDown()
+        {
+            Debug.Log("Sprite Clicked!"); // Replace with desired action
+            // Example: Destroy(gameObject);
+            // Example: GetComponent<SpriteRenderer>().color = Color.red;
+        }
+    }
